77331efc9d5f57a83a8a0b33daaa02ba0b274c63
johnmorris4 <johnmorris4@bellsouth.net>
2020-05-27 05:40:12 -0400

    Working on Generic Repos

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,8 @@ namespace API
         {
             services.AddControllers();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>),
+                (typeof(GenericRepository<>)));
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
         }
