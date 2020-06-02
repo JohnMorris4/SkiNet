c08594e811e38ced449dc25586553e8163a8dbc9
johnmorris4 <johnmorris4@bellsouth.net>
2020-06-02 04:57:14 -0400

    Started setting core project files and folders

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
+import {CoreModule} from "./core/core.module";
+import {ShopModule} from "./shop/shop.module";
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
+
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
